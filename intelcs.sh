997c2f019460a24b4a50362dd06b915ce26c1ad1
RosaLaRosa <77786217+RosaLaRosa@users.noreply.github.com>
2021-01-27 15:22:54 +0100

    Add files via upload

--- intelcs.sh
+++ intelcs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir /opt/custom/cliorepo/intelcs /opt/intelcs/
-mv KayJu/owt-ubuntu.zip /opt/custom/cliorepo/intelcs/
+mv ../KayJu/owt-ubuntu.zip /opt/custom/cliorepo/intelcs/
 unzip /opt/custom/cliorepo/intelcs/owt-ubuntu.zip -d /opt/intelcs/
 mv /opt/intelcs/dist/ /opt/intelcs/Release-v4.3.1-compilata
 ln -s /opt/intelcs/Release-v4.3.1-compilata/ /opt/intelcs/production
